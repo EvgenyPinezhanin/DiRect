74188f3ec9e1e3c3b33e0a6504835fb733a8d881
EvgenyPinezhanin <pinezhanin@yandex.ru>
2023-03-13 00:00:26 +0300

    add sovle_test and add/delete parameters

--- include/direct_method.h
+++ include/direct_method.h
@@ -16,8 +16,10 @@ private:
     vector<double> A, B;
 
     int n, max_feval, max_iter;
-    double magic_eps;
+    double magic_eps, magic_eps_abs;
     double volume_reltol, sigma_reltol;
+
+    double eps;
 
     FILE *logfile;
     direct_algorithm algorithm;
@@ -25,10 +27,10 @@ private:
 public:
     direct_method(direct_objective_func _f = nullptr, void *_f_data = nullptr, int _n = 2, vector<double> _A = vector<double>{}, 
                   vector<double> _B = vector<double>{}, int _max_feval = 1000, int _max_iter = 1000, double _magic_eps = 0.0, 
-                  double _volume_reltol = 0.0, double _sigma_reltol = -1.0, FILE *_logfile = nullptr, 
+                  double _magic_eps_abs = 0.0, double _volume_reltol = 0.0, double _sigma_reltol = -1.0, FILE *_logfile = nullptr, 
                   direct_algorithm _algorithm = DIRECT_ORIGINAL) : f(_f), f_data(_f_data), n(_n), A(_A), B(_B), 
-                  max_feval(_max_feval), max_iter(_max_iter), magic_eps(_magic_eps), volume_reltol(_volume_reltol), 
-                  sigma_reltol(_sigma_reltol), logfile(_logfile), algorithm(_algorithm) {};
+                  max_feval(_max_feval), max_iter(_max_iter), magic_eps(_magic_eps), magic_eps_abs(_magic_eps_abs), 
+                  volume_reltol(_volume_reltol), sigma_reltol(_sigma_reltol), logfile(_logfile), algorithm(_algorithm) {};
 
     void setF(direct_objective_func _f) { f = _f; };
     void setFData(void* _f_data) { f_data = _f_data; };
@@ -40,13 +42,15 @@ public:
     void setMaxFeval(int _max_feval) { max_feval = _max_feval; };
     void setMaxIter(int _max_iter) { max_iter = _max_iter; };
     void setMagicEps(double _magic_eps) { magic_eps = _magic_eps; };
-    void setVolumeReltol(int _volume_reltol) { volume_reltol = _volume_reltol; };
-    void setSigmaReltol(int _sigma_reltol) { sigma_reltol = _sigma_reltol; };
+    void setMagicEpsAbs(double _magic_eps_abs) { magic_eps_abs = _magic_eps_abs; };
+    void setVolumeReltol(double _volume_reltol) { volume_reltol = _volume_reltol; };
+    void setSigmaReltol(double _sigma_reltol) { sigma_reltol = _sigma_reltol; };
 
     void setLogfile(FILE *_logfile) { logfile = _logfile; };
     void setAlghorithm(direct_algorithm _algorithm) { algorithm = _algorithm; };
 
-    int solve(vector<double> &X, double &minf);
+    direct_return_code solve(vector<double> &X, double &minf);
+    direct_return_code solve_test();
 };
 
 #endif // DIRECT_METHOD_H
--- src/direct_method.cpp
+++ src/direct_method.cpp
@@ -2,18 +2,25 @@
 
 #include <cmath>
 
-int direct_method::solve(vector<double> &X, double &minf) {
+direct_return_code direct_method::solve(vector<double> &X, double &minf) {
     direct_return_code ret_code;
-    double magic_eps_abs = abs(magic_eps);
     double start = 0.0, maxtime = 0.0;
     int force_stop = 0;
-    double fglobal_reltol = 0.0;
+    double fglobal = DIRECT_UNKNOWN_FGLOBAL;
+    double fglobal_reltol = DIRECT_UNKNOWN_FGLOBAL_RELTOL;
 
     X.resize(n);
 
     ret_code = direct_optimize(f, f_data, n, A.data(), B.data(), X.data(), &minf, max_feval, max_iter, 
                                start, maxtime, magic_eps, magic_eps_abs, volume_reltol, sigma_reltol,
-                               &force_stop, DIRECT_UNKNOWN_FGLOBAL, fglobal_reltol, logfile, algorithm);
+                               &force_stop, fglobal, fglobal_reltol, logfile, algorithm);
     
     return ret_code;
+}
+
+direct_return_code direct_method::solve_test() {
+    vector<double> X(n);
+    double minf;
+
+    return solve(X, minf);
 }
